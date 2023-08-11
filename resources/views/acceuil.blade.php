4595b15bbceb8b02343b29cfb65c9636dc5503d1
pierredamien32 <orpheetohou16@gmail.com>
2023-08-11 13:22:16 +0100

    Mise en place du projet ClientTechLink

---
+++ resources/views/acceuil.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>ClientTechLink</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="{{ asset('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{ asset('assets/vendors/css/vendor.bundle.base.css')}}">
+    <!-- endinject -->
+    <!-- Plugin css for this page -->
+    <!-- End plugin css for this page -->
+    <!-- inject:css -->
+    <!-- endinject -->
+    <!-- Layout styles -->
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}">
+    <!-- End layout styles -->
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico')}}" />
+</head>
+<style>
+.style {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+
+<body class="bg-gradient-primary">
+    <div class="container d-flex justify-content-center align-items-center min-vh-100">
+        <div class="style">
+            <h1 class="text-white ">Mon Blog</h1>
+            <p class="text-white ">Ceci est la plateforme de support technique dédiée aux clients d'AnyxTech.</p>
+            <a href="#">Lire la suite</a>
+        </div>
+    </div>
+</body>
+
+</html>
