9c5e0af60fea520c56a988a00c59117aa190a6af
pierredamien32 <orpheetohou16@gmail.com>
2023-08-28 12:41:52 +0100

    Gestion d'erreur

---
+++ database/migrations/2023_08_28_110452_add_new_columns_to_sites_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sites', function (Blueprint $table) {
+            $table->float('local_latitude', 12, 10);
+            $table->float('local_longitude', 12, 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sites', function (Blueprint $table) {
+            //
+        });
+    }
+};
